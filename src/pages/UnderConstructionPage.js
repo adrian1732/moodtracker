0bb6d35b3cb867681a7afee558bd7460093970cd
Adrian <acadrian40@gmail.com>
2021-01-15 13:36:45 -0800

    under construction

---
+++ src/pages/UnderConstructionPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './UnderConstructionPage.css'
+
+//Page appears if page doesn't exist
+const UnderConstructionPage = () => (
+    <h1>This Page is under Construction</h1>
+);
+
+export default UnderConstructionPage;
